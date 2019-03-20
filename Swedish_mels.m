8bdbde77f1a416d02cade90ae66daaaaf9bc050e
Ben Lang <ben.lang@nyu.edu>
2019-03-20 21:27:39 +0400

    Update Swedish_mels.m

--- Swedish_mels.m
+++ Swedish_mels.m
@@ -1,6 +1,9 @@
-%% This script is intended to demonstrate a simple plotting of 3D point data. The dataset are frequency (Hz) measurements of formants F1, F2, and F3 of the Swedish vowel space, from Kuronen (2000).
-% The data were then converted into Mels as a respresentation of subjective distance. The .xlsx file has the data reported in Mels.
-% Ben Lang: ben.lang@nyu.edu
+%% This script is intended to demonstrate a simple plotting of 3D point data. The dataset are frequency (Hz) measurements of formants F1, F2, and F3 of the Swedish and American English vowel spaces, from Kuronen (2000).
+% The data were then converted into Mels as a respresentation of subjective
+% distance. The .xlsx files with "..._mels" have the data reported in Mels
+% and are pulled in via this script.
+% Ben Lang
+% ben.lang@nyu.edu
 
 %% Clear all output, close all windows, and clear the command line
 
@@ -10,44 +13,59 @@ clc;
 
 %% Read in the Swedish and American English vowel data and create necessary arrays and matrices as variables
 
+% Swedish
 swedish_vowels_mels = 'kuronen_mel.xlsx';
 [num,txt] = xlsread(swedish_vowels_mels); %reads in the numbers as a matrix and the text as ana array from the spreadsheet
 
-Mels = num;
+Mels = num; % rename variable
 vowel_label = txt;
 
-x = Mels(:,1);
+x = Mels(:,1); % pulls single column of data into x variable
 y = Mels(:,2);
 z = Mels(:,3);
-S = (600);
-s = S(:);
+
+% American English
+american_vowels_mels = 'hillenbrand_mels.xlsx';
+[num2,txt2] = xlsread(american_vowels_mels); %reads in the numbers as a matrix and the text as ana array from the spreadsheet
+
+Mels2 = num2; % rename variable
+vowel_label2 = txt2;
+
+x2 = Mels2(:,1); % pulls single column of data into x variable
+y2 = Mels2(:,2);
+z2 = Mels2(:,3);
+
+S = (400); % changes the size of the shape plotted for each point by creating a vector size
+s = S(:); % vector inserted into variable that teh function for plotting uses
 str = txt;
+str2 = txt2;
 
 
 
 %% Plot the data
-figure('units','normalized','outerposition',[0 0 1 1], 'color','[1 1 1]')
-scatter3(x, y, z, s, 'filled', 'd', 'markeredgecolor','k'); % plots the data as an open dot for each point
-title('Swedish Vowels in Mels')
-xlabel('F1')
-ylabel('F2')
-zlabel('F3')
-xpos = [x];
-ypos = [y];
-labels = vowel_label;
-labelpoints(xpos, ypos, labels)
-set(gca, 'fontsize', 24)
+figure('units','normalized','outerposition',[0 0 1 1], 'color','[1 1 1]') % fullscreen figure, centered, white background
+scatter3(x, y, z, s, 'filled', 'd', 'm', 'markeredgecolor','k'); % plots the Swedish data as a filled diamond for each point
+title('Swedish and American English Vowels in Mels') % title
+xlabel('F1') % x-axis label
+ylabel('F2') % y-axis label
+zlabel('F3') % z-axis label
+hold on %keeps first plot on the figure so you can add additional points
+scatter3(x2, y2, z2, s, 'filled', 'c','markeredgecolor','k'); % plots the American English data as a filled dot for each point
+text(x,y,z,txt,'FontSize',40,'Color','k') % plots Swedish phoneme on corresponding xyz coordinate as text, size 40, black
+text(x2,y2,z2,txt2,'FontSize',40,'Color','k') % plots American English phoneme on corresponding xyz coordinate as text, size 40, black
+set(gca, 'fontsize', 24) % sets overall figure font size to 24
 
 %% Plot the data
 
-figure('units','normalized','outerposition',[0 0 1 1], 'color','[1 1 1]')
-ts = textscatter3(x,y,z,str);
-set(ts, 'fontsize', 24)
-title('Swedish Vowels in Mels')
-xlabel('F1')
-ylabel('F2')
-zlabel('F3')
-set(gca, 'fontsize', 24)
+% figure('units','normalized','outerposition',[0 0 1 1], 'color','[1 1 1]')
+% ts = textscatter3(x,y,z,str);
+% set(ts, 'fontsize', 24)
+% title('Swedish Vowels in Mels')
+% xlabel('F1')
+% ylabel('F2')
+% zlabel('F3')
+% set(gca, 'fontsize', 24)
 
 %% Measure the linear distances between specific points of interest
+
 
